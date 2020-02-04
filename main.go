83c4e5d232230b0f40e3b36e2796386f930a8f2d
David Montoya <davidmontoyago@gmail.com>
2020-02-04 11:09:38 -0600

    prevent boundless connections/goroutines

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 	"os/signal"
@@ -13,15 +14,16 @@ func main() {
 
 	proxy := newProxy(":9000", balancer)
 
+	ctx, cancel := context.WithCancel(context.Background())
 	c := make(chan os.Signal)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
-		log.Println("waiting for in-flight connections...")
-		proxy.gracefulShutdown()
+		log.Println("received termination signal...")
+		cancel()
 		log.Println("shutting down now.")
 		os.Exit(0)
 	}()
 
-	proxy.listen()
+	proxy.listen(ctx)
 }
--- proxy.go
+++ proxy.go
@@ -1,25 +1,31 @@
 package main
 
 import (
+	"context"
 	"io"
 	"log"
 	"net"
 	"sync"
 )
 
+const maxConnections = 10000
+const maxConcurrent = 1000
+
 type proxy struct {
-	addr     string
-	listener net.Listener
-	balancer *roundRobinBalancer
-	clientWg *sync.WaitGroup
+	addr        string
+	listener    net.Listener
+	balancer    *roundRobinBalancer
+	clientWg    *sync.WaitGroup
+	clientConns chan net.Conn
 }
 
 func newProxy(addr string, balancer *roundRobinBalancer) *proxy {
 	var wg sync.WaitGroup
 	return &proxy{
-		addr:     addr,
-		balancer: balancer,
-		clientWg: &wg,
+		addr:        addr,
+		balancer:    balancer,
+		clientWg:    &wg,
+		clientConns: make(chan net.Conn, maxConnections),
 	}
 }
 
@@ -29,21 +35,49 @@ func (p *proxy) gracefulShutdown() {
 	p.balancer.shutdown()
 }
 
-func (p *proxy) listen() {
+func (p *proxy) listen(ctx context.Context) {
+	p.spawnConnectionHandlers()
+
+	// serves as proxy for client connections chan and allows shutting down gracefully
+	newClientChan := make(chan net.Conn, 1)
+	go func() {
+		defer close(p.clientConns)
+		for {
+			select {
+			case conn := <-newClientChan:
+				p.clientConns <- conn
+			case <-ctx.Done():
+				log.Println("waiting for in-flight connections...")
+				p.gracefulShutdown()
+				return
+			}
+		}
+	}()
+
 	var err error
 	p.listener, err = net.Listen("tcp", p.addr)
 	if err != nil {
-		log.Fatalln("unable to listen on host interface(s):", err)
+		log.Fatalln("unable to listen on host interface", err)
 	}
 
 	for {
 		conn, err := p.listener.Accept()
 		if err != nil {
-			log.Println("listener no longer accepting connections.", err)
-			break
+			log.Println("listener no longer accepting connections", err)
+			return
 		}
 		p.clientWg.Add(1)
-		go p.proxyConnection(conn)
+		newClientChan <- conn
+	}
+}
+
+func (p *proxy) spawnConnectionHandlers() {
+	for i := 0; i < maxConcurrent; i++ {
+		go func() {
+			for conn := range p.clientConns {
+				p.proxyConnection(conn)
+			}
+		}()
 	}
 }
 
